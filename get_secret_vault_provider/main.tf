4aab56b2dd24f0558316fbcd1fb87b72f983300a
chpag <115650339+chpag@users.noreply.github.com>
2025-02-10 22:47:24 +0100

    Update main.tf

--- get_secret_vault_provider/main.tf
+++ get_secret_vault_provider/main.tf
@@ -6,12 +6,16 @@ variable "secret_infra_path" {
   description = "Path of the Secret Engine where Infrastructure Secret are stored"
   type        = string
 }
-variable "secret_infra_path" {
+variable "secret_infra_name" {
   description = "Name of the Secret Engine where Infrastructure Secret are stored"
   type        = string
 }
 
-data "vault_kv_secret_v2" "example" {
+data "vault_kv_secret_v2" "infra_secrets" {
   mount = var.secret_infra_path
   name  = var.secret_infra_name
+}
+
+output "infra_secrets" {
+  values = data.vault_kv_secret_v2.infra_secrets.data
 }
