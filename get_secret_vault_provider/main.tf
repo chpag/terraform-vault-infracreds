59b2caeae48aa208c22cb17b285dcce54875d761
chpag <115650339+chpag@users.noreply.github.com>
2025-02-10 23:00:00 +0100

    Update main.tf

--- get_secret_vault_provider/main.tf
+++ get_secret_vault_provider/main.tf
@@ -1,5 +1,30 @@
+
+variable "vault_username"  {
+  description = "Vault Username"
+  type        = string
+}
+variable "vault_password" {
+  description = "Vault Password"
+  type        = string
+}
+variable "secret_infra_path" {
+  description = "Path of the Secret Engine where Infrastructure Secret are stored"
+  type        = string
+}
+variable "secret_infra_name" {
+  description = "Name of the Secret Engine where Infrastructure Secret are stored"
+  type        = string
+}
+
 provider "vault" {
-  auth_login = "userpass"
+  auth_login {
+    path = "auth/userpass/login/${var.vault_username}"
+
+    parameters = {
+      password = var.vault_password
+    }
+  }
+}
 }
 
 variable "secret_infra_path" {
